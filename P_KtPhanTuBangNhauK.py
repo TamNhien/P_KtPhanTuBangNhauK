00854a9e87bf5ee3e3054d1faa8a7877703af87c
TamNhien <canhthien91@gmail.com>
2024-03-27 19:13:03 +0700

    Add project files.

---
+++ P_KtPhanTuBangNhauK.py
@@ -0,0 +1,12 @@
+﻿import re
+
+
+def check_equal_elements(lst):
+    return all(elem == lst[0] for elem in lst)
+
+def main():
+    lst = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy : ").split(',')))
+    print(check_equal_elements(lst))
+
+if __name__ == "__main__":
+    main()
